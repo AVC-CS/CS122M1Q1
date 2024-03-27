7b3394f9b32072b8de4b6bf21ae3a2af9f01d9b6
Kyuwoong Lee <kwooler@gmail.com>
2024-03-26 17:17:53 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+def main():
+    number = []
+    evencnt = 0
+
+    for i in range(10):
+        number.append(int(input('Enter a number: ')))
+
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+    print(evencnt)
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return evencnt
+
+
+if __name__ == '__main__':
+    main()
