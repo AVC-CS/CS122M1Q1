dee893de10bf4e2d2e125250865a4e36cba8d4dd
Kyu Lee <kwooler@gmail.com>
2024-05-15 15:44:35 -0700

    M1Q1

--- main.py
+++ main.py
@@ -7,22 +7,12 @@ def main():
     for i in range(10):
         number.append(int(input('Enter a number: ')))
 
-    on = prev = 0
-    for i in range(len(number)):
-        if on == 0 and prev == 1 and number[i] % 2 == 0:
-            evencnt += 1
-            on = 1
-        if number[i] % 2 == 1:
-            on = 0
-
-        prev = 0 if number[i] % 2 else 1
-
-    print(evencnt)
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    print(evencnt)
 
     ########################################
     # Do not delete the return statement
