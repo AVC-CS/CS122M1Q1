9cff458fbf3cbc425d3ab43b59a2cb0867bcdf33
Kyu Lee <kwooler@gmail.com>
2024-05-15 15:42:31 -0700

    M1Q1

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 
 
 def main():
-    result = []
+    number = []
+    evencnt = 0
 
-    numbers = list(map(int, (input().split())))
+    for i in range(10):
+        number.append(int(input('Enter a number: ')))
 
+    on = prev = 0
+    for i in range(len(number)):
+        if on == 0 and prev == 1 and number[i] % 2 == 0:
+            evencnt += 1
+            on = 1
+        if number[i] % 2 == 1:
+            on = 0
+
+        prev = 0 if number[i] % 2 else 1
+
+    print(evencnt)
     """
     ########################################
     Code Your Program here
     ########################################
     """
-    print(result)
 
     ########################################
     # Do not delete the return statement
     ########################################
-    return result
+    return evencnt
 
 
 if __name__ == '__main__':
